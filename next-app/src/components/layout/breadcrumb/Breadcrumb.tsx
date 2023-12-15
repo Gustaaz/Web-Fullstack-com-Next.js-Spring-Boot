111f8ef84083e263e77d47269f029cf573cacb9a
Gustavo Guilherme Campelo Antunes <guguiantunes@hotmail.com>
2023-12-15 13:46:01 -0400

    upgrade: atualizado pra indentificar a rota de inicio como "/"

--- next-app/src/components/layout/breadcrumb/Breadcrumb.tsx
+++ next-app/src/components/layout/breadcrumb/Breadcrumb.tsx
@@ -8,17 +8,18 @@ export function Breadcrumb() {
   const currentPathname = usePathname()
   const pathnames = currentPathname.split('/').filter(Boolean)
 
+  pathnames.unshift('/')
   const linkClas =
     'inline-flex items-center gap-2 text-sm font-medium text-gray-700 dark:text-gray-600 hover:text-blue-600'
   const textClas = 'ml-1 text-sm font-medium text-gray-700 hover:text-blue-600 '
 
   const linkElements = pathnames.map((name, index) => {
-    const path = '/' + pathnames.slice(0, index + 1).join('/')
+    const path = pathnames.slice(0, index + 1).join('')
     const key = name === 'inicio' ? index : name
-    const isInicio = name === 'inicio'
+    const isInicio = name === '/'
 
     const component = isInicio ? (
-      <Link className={`${linkClas} `} href={path}>
+      <Link className={`${linkClas} `} href={'/'}>
         <Home size={20} /> Inicio
       </Link>
     ) : (
