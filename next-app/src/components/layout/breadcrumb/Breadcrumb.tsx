4cd85309da7b1f4894f52c4e9da6938a4c8f36d4
Gustavo Guilherme Campelo Antunes <guguiantunes@hotmail.com>
2023-12-15 13:07:10 -0400

    componente de Breadcrumb

---
+++ next-app/src/components/layout/breadcrumb/Breadcrumb.tsx
@@ -0,0 +1,47 @@
+'use client'
+
+import { usePathname } from 'next/navigation'
+import Link from 'next/link'
+import { ChevronRight, Home } from 'lucide-react'
+
+export function Breadcrumb() {
+  const currentPathname = usePathname()
+  const pathnames = currentPathname.split('/').filter(Boolean)
+
+  const linkClas =
+    'inline-flex items-center gap-2 text-sm font-medium text-gray-700 dark:text-gray-600 hover:text-blue-600'
+  const textClas = 'ml-1 text-sm font-medium text-gray-700 hover:text-blue-600 '
+
+  const linkElements = pathnames.map((name, index) => {
+    const path = '/' + pathnames.slice(0, index + 1).join('/')
+    const key = name === 'inicio' ? index : name
+    const isInicio = name === 'inicio'
+
+    const component = isInicio ? (
+      <Link className={`${linkClas} `} href={path}>
+        <Home size={20} /> Inicio
+      </Link>
+    ) : (
+      <div className="flex items-center">
+        <ChevronRight size={20} />
+        <Link href={path} className={`${textClas} dark:text-gray-600`}>
+          {name}
+        </Link>
+      </div>
+    )
+
+    return (
+      <li key={key} className={`${isInicio && 'inline-flex items-center'}`}>
+        {component}
+      </li>
+    )
+  })
+
+  return (
+    <nav className="mb-5 ml-2 flex flex-wrap md:ml-0" aria-label="Breadcrumb">
+      <ol className="flex flex-wrap items-center space-x-1 md:space-x-1">
+        {linkElements}
+      </ol>
+    </nav>
+  )
+}
