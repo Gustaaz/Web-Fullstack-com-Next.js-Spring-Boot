fe188d10e5ccf527382111a9754c676283853aea
Gustavo Guilherme Campelo Antunes <guguiantunes@hotmail.com>
2023-12-29 14:17:23 -0400

    alteração adptando o componente de textarea para se adptar ao react hook forms

--- next-app/src/components/inputs/Textarea.tsx
+++ next-app/src/components/inputs/Textarea.tsx
@@ -1,41 +1,53 @@
-import React, { TextareaHTMLAttributes } from 'react'
+import React, { TextareaHTMLAttributes, forwardRef } from 'react'
 
 type TextareaProps = TextareaHTMLAttributes<HTMLTextAreaElement> & {
   name: string
   label: string
   helpText?: string
   description?: string
+  length?: number
 }
 
-export const Textarea = ({
-  label = 'Textarea',
-  name = 'textarea',
-  helpText = '',
-  description = '',
-  ...props
-}: TextareaProps) => {
-  const helpTextColor = `${helpText ? 'text-red-500' : 'text-gray-600'}`
-  const helperTextBorder = `${helpText && 'border-red-300'} ${
-    helpText ? 'focus:border-red-500' : 'focus:border-gray-500'
-  } `
-  return (
-    <>
-      <label
-        className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-        htmlFor={name}
-      >
-        {label}
-      </label>
-      <textarea
-        {...props}
-        id={name}
-        name={name}
-        rows={6}
-        className={`appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white ${helperTextBorder}`}
-      />
-      <p className={`text-xs italic ${helpTextColor}`}>
-        {helpText || description}
-      </p>
-    </>
-  )
-}
+export const Textarea = forwardRef<HTMLTextAreaElement, TextareaProps>(
+  (
+    {
+      label = 'Textarea',
+      helpText = '',
+      description = '',
+      name = '',
+      length = 0,
+      ...rest
+    },
+    ref,
+  ) => {
+    const helpTextColor = `${helpText ? 'text-red-500' : 'text-gray-600'}`
+    const helperTextBorder = `${helpText && 'border-red-300'} ${
+      helpText ? 'focus:border-red-500' : 'focus:border-gray-500'
+    } `
+    return (
+      <>
+        <label
+          className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+          htmlFor={name}
+        >
+          <div className="flex justify-between">
+            {label}
+            <span>{length}/100</span>
+          </div>
+        </label>
+        <textarea
+          {...rest}
+          ref={ref}
+          maxLength={100}
+          rows={6}
+          name={name}
+          id={name}
+          className={`appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white ${helperTextBorder}`}
+        />
+        <p className={`text-xs italic ${helpTextColor}`}>
+          {helpText || description}
+        </p>
+      </>
+    )
+  },
+)
