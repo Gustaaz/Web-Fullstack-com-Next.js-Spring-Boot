874a381a27c8d959bdabc5d08ae12efd2e190546
Gustavo Guilherme Campelo Antunes <guguiantunes@hotmail.com>
2023-12-29 14:22:50 -0400

    adicionando margin a tabela

--- next-app/src/app/produtos/page.tsx
+++ next-app/src/app/produtos/page.tsx
@@ -4,16 +4,18 @@ import Link from 'next/link'
 export default function ProductListing() {
   return (
     <div className="rounded-sm border bg-white px-5 pt-6 pb-2.5 shadow-md sm:px-7.5 xl:pb-1">
-      <div className="mb-8 flex items-center justify-between">
-        <h4 className="text-xl font-semibold text-black">Produtos</h4>
-        <Link
-          href="/produtos/adicionar"
-          className="inline-flex items-center justify-center rounded-md border border-black py-2 px-10 text-center font-medium text-black hover:bg-gray-100 lg:px-4 xl:px-7"
-        >
-          Novo
-        </Link>
+      <div className="mb-8 lg:mx-16 mt-3 ">
+        <div className="mb-8 flex items-center justify-between">
+          <h4 className="text-xl font-semibold text-black">Produtos</h4>
+          <Link
+            href="/produtos/adicionar"
+            className="inline-flex items-center justify-center rounded-md border border-black py-2 px-10 text-center font-medium text-black hover:bg-gray-100 lg:px-4 xl:px-7"
+          >
+            Novo
+          </Link>
+        </div>
+        <Table />
       </div>
-      <Table />
     </div>
   )
 }
