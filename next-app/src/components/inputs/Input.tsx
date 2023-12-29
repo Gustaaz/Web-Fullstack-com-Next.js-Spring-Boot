c3db19b96c6db08d525dbbe856c84b4095564944
Gustavo Guilherme Campelo Antunes <guguiantunes@hotmail.com>
2023-12-29 14:15:52 -0400

    Alteração: adptando o componente do input para o react-hook-forms

--- next-app/src/components/inputs/Input.tsx
+++ next-app/src/components/inputs/Input.tsx
@@ -1,39 +1,47 @@
-import { InputHTMLAttributes } from 'react'
+import { InputHTMLAttributes, forwardRef } from 'react'
 
 type InputProps = InputHTMLAttributes<HTMLInputElement> & {
   label: string
-  name: string
   helpText?: string
   description?: string
 }
-export const Input = ({
-  label = '',
-  name = '',
-  helpText = '',
-  description = '',
-  ...props
-}: InputProps) => {
-  const helperTextBorder = `${helpText && 'border-red-300'} ${
-    helpText ? 'focus:border-red-500' : 'focus:border-gray-500'
-  } `
-  const helpTextColor = `${helpText ? 'text-red-500' : 'text-gray-600'}`
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  (
+    {
+      label = '',
+      helpText = '',
+      description = '',
+      name,
+      type = 'text',
+      ...rest
+    },
+    ref,
+  ) => {
+    const helperTextBorder = `${helpText && 'border-red-300'} ${
+      helpText ? 'focus:border-red-500' : 'focus:border-gray-500'
+    } `
+    const helpTextColor = `${helpText ? 'text-red-500' : 'text-gray-600'}`
 
-  return (
-    <>
-      <label
-        className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-        htmlFor={name}
-      >
-        {label}
-      </label>
-      <input
-        id={name}
-        {...props}
-        className={`appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white ${helperTextBorder}`}
-      />
-      <p className={`text-xs italic ${helpTextColor}`}>
-        {helpText || description}
-      </p>
-    </>
-  )
-}
+    return (
+      <>
+        <label
+          className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+          htmlFor={name}
+        >
+          {label}
+        </label>
+        <input
+          {...rest}
+          ref={ref}
+          id={name}
+          type={type}
+          name={name}
+          className={`appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white ${helperTextBorder}`}
+        />
+        <p className={`text-xs italic ${helpTextColor}`}>
+          {helpText || description}
+        </p>
+      </>
+    )
+  },
+)
