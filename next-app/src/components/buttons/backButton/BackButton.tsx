d21588be62f415ff46d9d049267b635e5d925cfc
Gustavo Guilherme Campelo Antunes <guguiantunes@hotmail.com>
2023-12-29 12:26:20 -0400

    Alteração: alterand o componente BackButton para se tornar mais generico

--- next-app/src/components/buttons/backButton/BackButton.tsx
+++ next-app/src/components/buttons/backButton/BackButton.tsx
@@ -2,11 +2,19 @@
 import { useRouter } from 'next/navigation'
 import { MoveLeft } from 'lucide-react'
 
-export function BackButton() {
+type BackButtonProps = {
+  children?: React.ReactNode
+}
+export function BackButton({ children }: BackButtonProps) {
   const { back } = useRouter()
   return (
-    <button onClick={back} className="hover:text-blue-600  ">
-      <MoveLeft className="m-auto" size={22} />
-    </button>
+    <div className="flex items-center gap-2">
+      <button onClick={back} className="hover:text-blue-600  ">
+        <MoveLeft className="m-auto" size={22} />
+      </button>
+      <h1 className="text-2xl font-semibold leading-tight text-gray-800">
+        {children}
+      </h1>
+    </div>
   )
 }
